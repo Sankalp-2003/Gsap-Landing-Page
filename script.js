73805ff10840df68ef80d684ec3afcd56c30b77c
Sankalp <sankalpkundapur2003@gmail.com>
2024-07-23 19:49:47 +0530

    completed

---
+++ script.js
@@ -0,0 +1,32 @@
+gsap.from("#img1",{
+  delay:0.4,
+  opacity:0,
+  duration:1,
+  y:60
+})
+gsap.from("#img2",{
+  delay:0.4,
+  opacity:0,
+  duration:1,
+  x:60
+})
+gsap.from("#img3",{
+  delay:0.4,
+  opacity:0,
+  duration:1,
+  y:-60
+})
+gsap.from("#main h1",{
+  delay:0.4,
+  opacity:0,
+  duration:1,
+})
+gsap.from("#page2 h5, #page2 h1, #aboutus",{
+  opacity:0,
+  stagger:0.4,
+  scrollTrigger:{
+    trigger:"#page2 h5",
+    scroller:"body",
+    start:"top 60%",
+  }
+})
